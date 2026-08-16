2d0108aa642462d0fb3f32ff34eb56f15ca0e725
goulartju <juliegoulart21@gmail.com>
2026-08-15 21:50:13 -0300

    feat: migration de atualização de matrícula

---
+++ Mov.Infrastructure/Migrations/20260816004437_MatriculaCamposOpcionais.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mov.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MatriculaCamposOpcionais : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AtualizadoEm",
+                table: "Matriculas",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "AtualizadoEm",
+                table: "Matriculas",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
