d97add614c98bf5e9d2b22e933325f4f6394e66a
goulartju <juliegoulart21@gmail.com>
2026-09-05 12:17:35 -0300

    fix: aumentando o tempo de token

--- Mov.Domain/Settings/JwtSettings.cs
+++ Mov.Domain/Settings/JwtSettings.cs
@@ -5,6 +5,6 @@ public class JwtSettings
     public string Secret { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
-    public int AccessTokenExpirationMinutes { get; set; } = 15;
+    public int AccessTokenExpirationMinutes { get; set; } = 480;
     public int RefreshTokenExpirationDays { get; set; } = 7;
 }
